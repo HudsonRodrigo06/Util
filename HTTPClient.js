89d3658edbb71b6a3da11c8ca8ce15c7948b1f0d
Hudson <hudsonrodrigo06@gmail.com>
2020-11-10 20:36:19 -0300

    add

---
+++ HTTPClient.js
@@ -0,0 +1,64 @@
+﻿let HTTPClient = {
+
+	post: async function (url, obj) {
+
+		HTTPClient.toggleLoading();
+
+		let config = {
+			method: "POST",
+			body: JSON.stringify(obj),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		}
+
+		let p = await fetch(url, config);
+
+		HTTPClient.toggleLoading();
+
+		return p;
+	},
+
+	get: async function (url) {
+
+
+		let config = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		}
+
+		HTTPClient.toggleLoading();
+
+		let p = await fetch(url, config);
+
+		HTTPClient.toggleLoading();
+
+		return p;
+	},
+
+	toggleLoading: () => {
+
+		var divExiste = document.getElementById("divLoading");
+
+		if (divExiste == null) {
+
+			let div = document.createElement('div');
+			div.id = "divLoading";
+			div.className = "container-loading";
+
+			let img = document.createElement('img');
+			img.src = "/images/gifs/loading.gif";
+
+			div.appendChild(img);
+			document.body.appendChild(div);
+		}
+		else {
+			document.body.removeChild(divExiste);
+		}
+		
+
+	}
+
+}
---
+++ MyAlerts.js
@@ -0,0 +1,51 @@
+﻿let myalert = {
+
+	
+
+	// mensagem de erro
+	errorMsg: function (msg) {
+		Swal.fire({
+			icon: 'error',
+			title: 'Oops...',
+			text: msg
+		})
+	},
+
+	// mensagem de sucesso com redirect
+	sucessMsg: function (msg, redirect) {
+		Swal.fire(
+			'Good job!',
+			msg,
+			'success'
+		).then(function () {
+			window.location = redirect;
+		})
+
+	},
+
+	// apenas mensagem de sucesso
+	onlySucessMsg: function (msg) {
+		Swal.fire(
+			'Good job!',
+			msg,
+			'success'
+		).then(() => {
+
+			var fancyExiste = document.getElementsByClassName("fancybox-iframe");
+
+			if (fancyExiste != null)
+				window.parent.$.fancybox.close();
+
+		})
+	},
+
+	// apenas mensagem de sucesso
+	asyncOnlySucessMsg: async function (msg) {
+		Swal.fire(
+			'Good job!',
+			msg,
+			'success'
+		)
+	}
+
+}
